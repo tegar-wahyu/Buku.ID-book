33890248290a16c710b489c64dc362be747ba986
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-05-01 19:34:54 +0700

    feat: config with env file

--- src/test/java/id/ac/ui/cs/advprog/book/BookApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/book/BookApplicationTests.java
@@ -2,8 +2,10 @@ package id.ac.ui.cs.advprog.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookApplicationTests {
 
 	@Test
