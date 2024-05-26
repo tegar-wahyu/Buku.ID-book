1248e689c674331072f1194e3eede83b58ba29ac
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-05-26 15:27:23 +0700

    fix: context load

--- src/test/java/id/ac/ui/cs/advprog/book/BookApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/book/BookApplicationTests.java
@@ -1,9 +1,6 @@
 package id.ac.ui.cs.advprog.book;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -12,11 +9,12 @@ import org.springframework.test.context.ActiveProfiles;
 //@SpringBootTest
 class BookApplicationTests {
 
-  @Autowired
-  private BookApplication bookApplication;
-
 	@Test
 	void contextLoads() {
-    assertNotNull(bookApplication);
+	}
+
+	@Test
+	void testMain() {
+		BookApplication.main(new String[]{"args"});
 	}
 }
